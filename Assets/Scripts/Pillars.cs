98d4ce8a9ecb460d02048de22762905cc1b9d2ed
Laura S <70970443+LSpryte@users.noreply.github.com>
2020-10-01 21:46:26 -0400

    CompletelyRevampedScene

---
+++ Assets/Scripts/Pillars.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pillars : MonoBehaviour
+{
+    [SerializeField] float raiseHeight = 0f;
+    [SerializeField] float startingHeight = -7.15f;
+
+    [SerializeField] float raiseSpeed;
+
+    public bool raising;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.position = new Vector3(transform.position.x, startingHeight, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.localPosition.y >= raiseHeight)
+        {
+            raising = false;
+        }
+        if (transform.localPosition.y <= startingHeight)
+        {
+            raising = true;
+        }
+
+        if(raising)
+        {
+            transform.position += new Vector3(0, raiseSpeed *Time.deltaTime,0);
+        }
+        else
+        {
+            transform.position -= new Vector3(0, raiseSpeed * Time.deltaTime,0);
+        }
+
+        
+    }
+}
