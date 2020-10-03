eb5f159caa342e36a98c5d618176c18dd5914b2a
brandon-lcad <bcluff@lcad.edu>
2020-10-02 21:18:02 -0700

    Class #5

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,10 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public Obstacle[] obstacles; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        obstacles = FindObjectsOfType<Obstacle>();
+
+        foreach (Obstacle obstacle in obstacles) {
+            Debug.Log("Setting Obstacle: " + obstacle.gameObject.name);
+            obstacle.tag = "Obstacle";
+            obstacle.gameObject.layer = LayerMask.NameToLayer("Obstacle");
+        } 
     }
 
     // Update is called once per frame
