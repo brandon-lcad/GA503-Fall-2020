a49a103a31ddb080498787e5d8a8a7df13e80ecf
Parkachu0 <Parkachu0@gmail.com>
2020-10-09 17:34:05 -0700

    forgot to check all the boxes

---
+++ Assets/Scripts/ParkerScripts/PCGravityOrbit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCGravityOrbit : MonoBehaviour
+{
+
+    //the center that all objects orbit
+    public float Gravity;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PCGravityControl>())
+        {
+            //if this object has a gravity script, set this as the planet
+            other.GetComponent<PCGravityControl>().Gravity = this.GetComponent<PCGravityOrbit>();
+        }
+    }
+}
---
+++ Assets/Scripts/ParkerScripts/PCPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCPlayer : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
