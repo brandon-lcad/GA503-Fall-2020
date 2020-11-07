82491829fb920e77789681ce077de916de58f5ce
brandon-lcad <bcluff@lcad.edu>
2020-11-06 21:37:48 -0800

    Prototype For Parker

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+
+public class Movement : MonoBehaviour {
+    public float speed = 800.0f;
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/MovementFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementFollow : MonoBehaviour {
+    public GameObject follow;
+    private Vector3 offset; 
+    float distance;
+    Vector3 lastPosition, moveDirection;
+
+    void Start() {
+        offset = transform.position - follow.transform.position; 
+        distance = offset.magnitude;
+        lastPosition = follow.transform.position;
+    }
+
+    void LateUpdate() {
+        moveDirection = follow.transform.position - lastPosition;
+
+        if (moveDirection != Vector3.zero) { 
+            moveDirection = moveDirection.normalized;
+            transform.position = follow.transform.position - moveDirection * distance;
+            transform.LookAt(follow.transform.position); 
+            lastPosition = follow.transform.position;
+        }
+    }
+
+}
